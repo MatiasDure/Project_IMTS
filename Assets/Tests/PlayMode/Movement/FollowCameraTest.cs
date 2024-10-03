9e87c622d56ab0ce9a1c6db799cbc922b18efa6c
Matias Dure <497232@student.saxion.nl>
2024-10-03 18:29:18 +0200

    Updated failing test

--- Assets/Tests/PlayMode/Movement/FollowCameraTest.cs
+++ Assets/Tests/PlayMode/Movement/FollowCameraTest.cs
@@ -22,21 +22,22 @@ public class FollowCameraTest
 		followConfiguration.Target.position = new Vector3(3, 0, 4);
 
 		// Mock passage of time by manually calling LateUpdate
-        float simulatedTime = 10f;
-        float fpsToMS = 1f / 60f; // 60 frames per second to ms
-        int steps = Mathf.CeilToInt(simulatedTime / fpsToMS);
+        float simulatedTime = 15f;
+		float fixedDeltaTime = 1f / 60f; // Fixed deltaTime for each iteration (60 FPS equivalent)
+        Time.fixedDeltaTime = fixedDeltaTime; // Set fixedDeltaTime
+        int steps = Mathf.CeilToInt(simulatedTime / fixedDeltaTime);
 
         for (int i = 0; i < steps; i++) // Around 600 frames
         {
             followCamera.LateUpdate();
-            yield return null; // Wait for the next frame
+            yield return new WaitForFixedUpdate();
         }
 		
         Vector3 expectedPosition = followConfiguration.Target.position + followConfiguration.Target.forward * followConfiguration.Distance + followConfiguration.Offset;
-        Assert.IsTrue(IsApproximatelyEqual(followCamera.transform.position, expectedPosition));
+        Assert.IsTrue(IsApproximatelyEqual(followCamera.transform.position, expectedPosition), $"Expected: {expectedPosition}, But was: {followCamera.transform.position}");
 	} 
 
-	private bool IsApproximatelyEqual(Vector3 a, Vector3 b, float tolerance = 0.3f)
+	private bool IsApproximatelyEqual(Vector3 a, Vector3 b, float tolerance = 0.4f)
 	{
 		return Mathf.Abs(a.x - b.x) < tolerance && Mathf.Abs(a.y - b.y) < tolerance && Mathf.Abs(a.z - b.z) < tolerance;
 	}
